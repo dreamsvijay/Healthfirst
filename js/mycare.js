a75f599331fa4e9b354e2b29b0044e44bd51cdcc
DreamGuy's <dreamguystech@gmail.com>
2017-04-17 17:32:15 +0530

    Updated - v17-1

--- js/mycare.js
+++ js/mycare.js
@@ -4,21 +4,21 @@ $(".user-name").empty().append(window.localStorage.getItem("pat_name"));
 $(".user-number").empty().append(window.localStorage.getItem("pat_phone"));
 $(".user-dob").empty().append(window.localStorage.getItem("pat_dob"));
 $(document).ready(function(e) {
-    setTimeout(function(){ $("#loading").hide(); },100);
+    $("#loading").show();
 	if(!window.localStorage.getItem("pat_id")){ window.location.href="index.html"; }
-	/*$.post(base_url+"mobile-app?page=mycareteam",{pat_id:window.localStorage.getItem("pat_id"),pat_acctok:window.localStorage.getItem("pat_acctok"),pat_reftok:window.localStorage.getItem("pat_reftok")},
+	$.post(base_url+"mobile-app?page=mycareteam",{pat_id:window.localStorage.getItem("pat_id"),pat_acctok:window.localStorage.getItem("pat_acctok"),pat_reftok:window.localStorage.getItem("pat_reftok")},
 	function(data){
 		$("#loading").hide();
 		if(data.success == "Y"){
 			for(var i=0;i<data.data.length;i++){
 				$(".mycare-list").append('<li class="media"><a href="javascript:void(0);" class="media-link"><div class="media-left"><img src="img/profile-img.png" class="care-dr-img" alt="" width="47" height="47"></div><div class="media-body media-middle text-nowrap">								<div class="care-dr-name">'+data.data[i]['doctor_first_name']+" "+data.data[i]['doctor_last_name']+'</div><span class="speciality">'+data.data[i]['doctor_specialty']+'</span></div></a></li>');
 			}
-			 
 			
 		}else{
-			$(".mycare-list").append('<li class="media">No Results</li>');
+			$(".mycare-list").append('<li class="media"><a href="#" class="media-link">No Appointments</a></li>');
 		}
-	},"json");*/
+		$("#loading").hide();
+	},"json");
 	
 });
 
